0e470b13b0c8a373356f6a1e3b3db41ca879e63d
Leon Shan <144356027+Holycious@users.noreply.github.com>
2025-02-23 19:19:36 +0700

    Jobsheet Minggu 2 | Praktikum 2 -3 | Save Step Routing and Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return "Leon Shan Yoedha Adjie (2341720136)";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
+/*s
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
@@ -13,6 +16,70 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PageController::class,'index']);
+
+Route::get('/about', [PageController::class,'about']);
+
+Route::get('/articles/{id}', [PageController::class,'articles']);
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+   ]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+   ]);
+Route::get('/world', function () {
+    return 'World';
 });
+
+Route::get('/welcome', function () {
+    return 'Selamat Datang';
+});
+
+// Route::get('/about', function () {
+//     return 'Leon Shan Yoedha Adjie (2341720136)';
+// });
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya '.$name;
+//     });
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+// Route::get('/articles/{ID}', function ($ID) {
+//     return 'Halaman Artikel dengan ID ' . $ID;
+// });
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
+// Route::middleware(['first', 'second'])->group(function () {
+//     Route::get('/', function () {
+//         // Uses first & second middleware...
+//     });
+//     Route::get('/user/profile', function () {
+//         // Uses first & second middleware...
+//     });
+// });
+// Route::domain('{account}.example.com')->group(function () {
+//     Route::get('user/{id}', function ($account, $id) {
+//         //
+//     });
+// });
+// Route::middleware('auth')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+// });
